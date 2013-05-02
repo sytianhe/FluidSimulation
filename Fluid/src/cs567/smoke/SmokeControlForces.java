7bf5f2211a934e50a5efeecb98393c461335e4fa
Michael Flashman <michael.flashman@gmail.com>
2013-05-01 20:04:17 -0400

    making smoke move

--- Fluid/src/cs567/smoke/SmokeControlForces.java
+++ Fluid/src/cs567/smoke/SmokeControlForces.java
@@ -8,111 +8,111 @@ package cs567.smoke;
  */
 public class SmokeControlForces
 {
-    int         n, size;
+	int         n, size;
 
-    /** Temporary blurring field */
-    float[]     rhoBlur;
+	/** Temporary blurring field */
+	float[]     rhoBlur;
 
-    /** Current active keyframe. */
-    SmokeKeyframe keyframe = null;
+	/** Current active keyframe. */
+	SmokeKeyframe keyframe = null;
 
-    /** Builds forces for the specified solver. */
-    SmokeControlForces() //FluidSolver solver)
-    {
-	n    = Constants.n;
-	size = Constants.size; 
+	/** Builds forces for the specified solver. */
+	SmokeControlForces() //FluidSolver solver)
+	{
+		n    = Constants.n;
+		size = Constants.size; 
 
-	Blur.init(n, size);
+		Blur.init(n, size);
 
- 	rhoBlur = new float[size];
-    }
-
-    /** Specifies active/current key frame. */
-    public void setKeyframe(SmokeKeyframe K)
-    {
-	keyframe = K;
-	System.out.println("SmokeControlForces: keyframe = "+K);
-    }
-
-
-    /** 
-     * Smoke control driving force. 
-     */
-    public void getDrivingForce(float[] fx, float[] fy, float[] rho)
-    {
-	if(rho.length != fx.length) throw new IllegalArgumentException("density.length != fx.length");
-
- 	/// ZERO (fx,fy):
- 	for(int i=0; i<size; i++)   fx[i] = fy[i] = 0.f;
-
-	/// ZERO IF NO KEYFRAME:
-	if(keyframe==null) return;
-
-	SmokeKeyframe currentKeyframe = keyframe; /// cache reference in case it changes!
-
-	/// BLUR DENSITY:
-	Blur.blur(rho, rhoBlur);
-
-	/// COMPUTE FORCE: 
-	for(int i=1; i<=n; i++) {
-	    for(int j=1; j<=n; j++) {
-		int ij = I(i,j);
-		fx[ij] = Constants.V_f * 0;/// FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		fy[ij] = Constants.V_f * 0;/// FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	    }
+		rhoBlur = new float[size];
 	}
-    }
 
-    /** Evalutes gathering rate: rate = V_g * G = V_g * DIV ( D GRAD
-     * (rho-rhoGoal) ), where the diffusion coefficient is D =
-     * rho*rhoGoalBlur. */
-    public void getGatheringRate(float[] rate, float[] rho)
-    {
- 	/// ZERO rate (SLOTH: only need to zero boundary):
- 	for(int i=0; i<size; i++)   rate[i] = 0; 
-
-	/// ZERO IF NO KEYFRAME:
-	if(keyframe==null) return;
-
-	SmokeKeyframe currentKeyframe = keyframe; /// cache reference in case it changes!
-
-	float[] rhoGoal     = currentKeyframe.rhoGoal;
-	float[] rhoGoalBlur = currentKeyframe.rhoGoalBlur;
-
-	float something = 0;// ;)
-	for(int i=1; i<=n; i++) {
-	    for(int j=1; j<=n; j++) {
-
-		// GRAD(rho - rhoGoal)  (ON EDGES, x,y,X,Y):
-		float g_x = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		float g_X = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		float g_y = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		float g_Y = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-		// DIFFUSION COEFF (AVERAGE ON EDGES, x,y,X,Y):
-		float D_x = something;/// FIX! 
-		float D_X = something;/// FIX! 
-		float D_y = something;/// FIX! 
-		float D_Y = something;/// FIX! 
-
-		// DIV H = DIV (D GRAD(rho-rhoGoal)) (USING EDGE-BASED H=D GRAD(...)):
-		rate[I(i,j)] = Constants.V_g * something;///!!!!!!!!!!!!!!!!!!!!!!!
-	    }
+	/** Specifies active/current key frame. */
+	public void setKeyframe(SmokeKeyframe K)
+	{
+		keyframe = K;
+		System.out.println("SmokeControlForces: keyframe = "+K);
 	}
-    }
-
-    /** Normalizes density sum to match keyframe density sum. To avoid
-     * sudden changes in brightness, a gain is used to make the change
-     * gradual. */
-    public void normalizeDensity(float[] rho)//...)
-    {
-	/// IGNORE IF NO KEYFRAME:
-	if(keyframe==null) return;
-
-	/// DO SOMETHING HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    }
 
 
-    private final int I(int i, int j) { return Constants.I(i,j); }
+	/** 
+	 * Smoke control driving force. 
+	 */
+	public void getDrivingForce(float[] fx, float[] fy, float[] rho)
+	{
+		if(rho.length != fx.length) throw new IllegalArgumentException("density.length != fx.length");
+
+		/// ZERO (fx,fy):
+		for(int i=0; i<size; i++)   fx[i] = fy[i] = 0.f;
+
+		/// ZERO IF NO KEYFRAME:
+		if(keyframe==null) return;
+
+		SmokeKeyframe currentKeyframe = keyframe; /// cache reference in case it changes!
+
+		/// BLUR DENSITY:
+		Blur.blur(rho, rhoBlur);
+
+		/// COMPUTE FORCE: 
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				int ij = I(i,j);
+				fx[ij] = Constants.V_f * rhoBlur[ij] *  currentKeyframe.fdx[ij] ;
+				fy[ij] = Constants.V_f * rhoBlur[ij] * currentKeyframe.fdy[ij] ;
+			}
+		}
+	}
+
+	/** Evalutes gathering rate: rate = V_g * G = V_g * DIV ( D GRAD
+	 * (rho-rhoGoal) ), where the diffusion coefficient is D =
+	 * rho*rhoGoalBlur. */
+	public void getGatheringRate(float[] rate, float[] rho)
+	{
+		/// ZERO rate (SLOTH: only need to zero boundary):
+		for(int i=0; i<size; i++)   rate[i] = 0; 
+
+		/// ZERO IF NO KEYFRAME:
+		if(keyframe==null) return;
+
+		SmokeKeyframe currentKeyframe = keyframe; /// cache reference in case it changes!
+
+		float[] rhoGoal     = currentKeyframe.rhoGoal;
+		float[] rhoGoalBlur = currentKeyframe.rhoGoalBlur;
+
+		float something = 0;// ;)
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+
+				// GRAD(rho - rhoGoal)  (ON EDGES, x,y,X,Y):
+				float g_x = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				float g_X = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				float g_y = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				float g_Y = something;///FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+				// DIFFUSION COEFF (AVERAGE ON EDGES, x,y,X,Y):
+				float D_x = something;/// FIX! 
+				float D_X = something;/// FIX! 
+				float D_y = something;/// FIX! 
+				float D_Y = something;/// FIX! 
+
+				// DIV H = DIV (D GRAD(rho-rhoGoal)) (USING EDGE-BASED H=D GRAD(...)):
+				rate[I(i,j)] = Constants.V_g * something;///!!!!!!!!!!!!!!!!!!!!!!!
+			}
+		}
+	}
+
+	/** Normalizes density sum to match keyframe density sum. To avoid
+	 * sudden changes in brightness, a gain is used to make the change
+	 * gradual. */
+	public void normalizeDensity(float[] rho)//...)
+	{
+		/// IGNORE IF NO KEYFRAME:
+		if(keyframe==null) return;
+
+		/// DO SOMETHING HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	}
+
+
+	private final int I(int i, int j) { return Constants.I(i,j); }
 
 }
--- Fluid/src/cs567/smoke/SmokeKeyframe.java
+++ Fluid/src/cs567/smoke/SmokeKeyframe.java
@@ -11,127 +11,134 @@ import javax.imageio.*;
  * resized to a square grid of resolution specified in Constants.
  * 
  * @author Doug James, April 2007.
-*/
+ */
 public class SmokeKeyframe
 {
-    /** Keyframe image loaded. */
-    private String imageFilename;
+	/** Keyframe image loaded. */
+	private String imageFilename;
 
-    private int      N;
-    private int      n;
-    private int      size;
+	private int      N;
+	private int      n;
+	private int      size;
 
-    /** Goal density of this keyframe. */
-    float[]  rhoGoal;
+	/** Goal density of this keyframe. */
+	float[]  rhoGoal;
 
-    /** Blurred goal density. */
-    float[]  rhoGoalBlur;
+	/** Blurred goal density. */
+	float[]  rhoGoalBlur;
 
-    /** Constant part of driving-force field:  <code>(fdx,fdy) = (GRAD rhoGoalBlur)/rhoGoalBlur.</code> */
-    float[]  fdx, fdy;
+	/** Constant part of driving-force field:  <code>(fdx,fdy) = (GRAD rhoGoalBlur)/rhoGoalBlur.</code> */
+	float[]  fdx, fdy;
 
-    /** sum(rhoGoal) in case you need it ;) */
-    float   rhoGoalSum;
+	/** sum(rhoGoal) in case you need it ;) */
+	float   rhoGoalSum;
 
-    /**
-     * Constructs a "smokey frame" from the specified image filename.
-     * 
-     * @param imageFilename Image in question. Resized, resampled, and
-     * converted to grayscale image.
-     */
-    SmokeKeyframe(String imageFilename) throws IOException
-    {
-	this.imageFilename = imageFilename;
+	/**
+	 * Constructs a "smokey frame" from the specified image filename.
+	 * 
+	 * @param imageFilename Image in question. Resized, resampled, and
+	 * converted to grayscale image.
+	 */
+	SmokeKeyframe(String imageFilename) throws IOException
+	{
+		this.imageFilename = imageFilename;
 
-	n    = Constants.n;
-	N    = n+2;
-	size = N*N;/// NUMBER OF PIXELS
+		n    = Constants.n;
+		N    = n+2;
+		size = N*N;/// NUMBER OF PIXELS
 
-	/// LOAD & RESAMPLE IMAGE FOR DENSITY KEYFRAME (rhoGoal):
-	rhoGoal = loadKeyframeDensity(imageFilename);
+		/// LOAD & RESAMPLE IMAGE FOR DENSITY KEYFRAME (rhoGoal):
+		rhoGoal = loadKeyframeDensity(imageFilename);
 
-	/// BLUR rhoGoal:
-	rhoGoalBlur = new float[size];
-	Blur.blur(rhoGoal, rhoGoalBlur);
+		/// BLUR rhoGoal:
+		rhoGoalBlur = new float[size];
+		Blur.blur(rhoGoal, rhoGoalBlur);
 
-	/// CACHE CONSTANT DRIVING FORCE PART: fd = (GRAD rhoGoalBlur)/rhoGoalBlur:
-	fdx = new float[size];
-	fdy = new float[size];
-	{/// TODO:
-	    for(int i=1; i<=n; i++) {/// IGNORE EDGE FORCE (i=0,n+1)
-		for(int j=1; j<=n; j++) {/// IGNORE EDGE FORCE (j=0,n+1)
+		/// CACHE CONSTANT DRIVING FORCE PART: fd = (GRAD rhoGoalBlur)/rhoGoalBlur:
+		fdx = new float[size];
+		fdy = new float[size];
+		{/// TODO:
+			for(int i=1; i<=n; i++) {/// IGNORE EDGE FORCE (i=0,n+1)
+				for(int j=1; j<=n; j++) {/// IGNORE EDGE FORCE (j=0,n+1)
 
-		    // COMPUTE  (fdx,fdy) = GRAD_rhoGoalBlur / rhoGoalBlur   (Don't divide by zero!)
-		    fdx[I(i,j)] = 0;/// FIX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		    fdy[I(i,j)] = 0;/// FIX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+					// COMPUTE  (fdx,fdy) = GRAD_rhoGoalBlur / rhoGoalBlur   (Don't divide by zero!)
+					if (rhoGoalBlur[I(i,j)] != 0f){
+						fdx[I(i,j)] =  ( rhoGoalBlur[I(i+1,j)] - rhoGoalBlur[I(i-1,j)] ) / 2f  /  rhoGoalBlur[I(i,j)] ;
+						fdy[I(i,j)] = ( rhoGoalBlur[I(i,j+1)] - rhoGoalBlur[I(i,j-1)] ) / 2f  /  rhoGoalBlur[I(i,j)] ;
+
+					}
+					else {
+						fdx[I(i,j)] = 0; 
+						fdy[I(i,j)] = 0;
+					}
+				}
+			}
 		}
-	    }
+
+		/// RECORD DENSITY SUM FOR SMOKE-MASS NORMALIZATION (in case you want it)
+		rhoGoalSum = Utils.sum(rhoGoal);
+		if(rhoGoalSum==0) throw new RuntimeException("Density keyframe had zero smoke mass... "+
+				"what kind of smoke keyframer are you!?!");
 	}
 
-	/// RECORD DENSITY SUM FOR SMOKE-MASS NORMALIZATION (in case you want it)
-	rhoGoalSum = Utils.sum(rhoGoal);
-	if(rhoGoalSum==0) throw new RuntimeException("Density keyframe had zero smoke mass... "+
-						     "what kind of smoke keyframer are you!?!");
-    }
-
-    public String toString() { 
-	return "SmokeKeyframe: "+imageFilename;
-    }
-
-    /** Returns keyframe density value at specified (i,j) location.  */
-    public float getDensity(int i, int j) { return rhoGoal[I(i,j)]; }
-
-    // util method for indexing 1d arrays
-    private final int I(int i, int j){ return Constants.I(i,j); }
-
-
-    /** Loads and resamples image to required image resolution
-     * specified by Constants. */
-    static float[] loadKeyframeDensity(String imageFilename) throws IOException
-    {
-	int N = Constants.N;
-	float[] density = new float[Constants.size];
-
-	/// LOAD IMAGE:
-	File file = new File(imageFilename);
-	if(!file.exists()) throw new FileNotFoundException("imageFilename="+imageFilename);
-	BufferedImage image = ImageIO.read(file);
-
-	/// RENDER TO KEYFRAME-RESOLUTION BufferedImage:
-	Image renderImage = Toolkit.getDefaultToolkit().createImage
-	    (new FilteredImageSource(image.getSource(), new AreaAveragingScaleFilter(N,N))); 
-	BufferedImage keyframeImage = new BufferedImage(N,N,BufferedImage.TYPE_BYTE_GRAY); //INT_RGB);
-	Graphics      gfx           = keyframeImage.getGraphics();
-	gfx.drawImage(renderImage,0,0,null);
-	gfx.dispose();
-	//ImageIO.write(keyframeImage, "png", new File(imageFilename+".keyframe.png"));
-
-	/// EXTRACT "density" DATA FROM keyframeImage:
-	Raster raster = keyframeImage.getData();
-	for(int i=0; i<N; i++) {
-	    for(int j=0; j<N; j++) {
-		//int fi = (N-1) - i;
-		int fj = (N-1) - j;
-		float gray = raster.getSampleFloat(i, fj, 0) / 255f;
-		density[Constants.I(i,j)] = gray;
-	    }
+	public String toString() { 
+		return "SmokeKeyframe: "+imageFilename;
 	}
 
-	return density;
-    }
+	/** Returns keyframe density value at specified (i,j) location.  */
+	public float getDensity(int i, int j) { return rhoGoal[I(i,j)]; }
 
-    /** Test code. */
-    public static void main(String[] args) 
-    {
-	try{
-	    System.out.println("Usage: args[0]=imageFilename");
-	    String imageFilename = args[0];
-	    SmokeKeyframe key = new SmokeKeyframe(imageFilename);
+	// util method for indexing 1d arrays
+	private final int I(int i, int j){ return Constants.I(i,j); }
 
-	}catch(Exception e) {
-	    e.printStackTrace();
-	    System.out.println("OOPS: "+e);
+
+	/** Loads and resamples image to required image resolution
+	 * specified by Constants. */
+	static float[] loadKeyframeDensity(String imageFilename) throws IOException
+	{
+		int N = Constants.N;
+		float[] density = new float[Constants.size];
+
+		/// LOAD IMAGE:
+		File file = new File(imageFilename);
+		if(!file.exists()) throw new FileNotFoundException("imageFilename="+imageFilename);
+		BufferedImage image = ImageIO.read(file);
+
+		/// RENDER TO KEYFRAME-RESOLUTION BufferedImage:
+		Image renderImage = Toolkit.getDefaultToolkit().createImage
+				(new FilteredImageSource(image.getSource(), new AreaAveragingScaleFilter(N,N))); 
+		BufferedImage keyframeImage = new BufferedImage(N,N,BufferedImage.TYPE_BYTE_GRAY); //INT_RGB);
+		Graphics      gfx           = keyframeImage.getGraphics();
+		gfx.drawImage(renderImage,0,0,null);
+		gfx.dispose();
+		//ImageIO.write(keyframeImage, "png", new File(imageFilename+".keyframe.png"));
+
+		/// EXTRACT "density" DATA FROM keyframeImage:
+		Raster raster = keyframeImage.getData();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				//int fi = (N-1) - i;
+				int fj = (N-1) - j;
+				float gray = raster.getSampleFloat(i, fj, 0) / 255f;
+				density[Constants.I(i,j)] = gray;
+			}
+		}
+
+		return density;
 	}
-    }
+
+	/** Test code. */
+	public static void main(String[] args) 
+	{
+		try{
+			System.out.println("Usage: args[0]=imageFilename");
+			String imageFilename = args[0];
+			SmokeKeyframe key = new SmokeKeyframe(imageFilename);
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("OOPS: "+e);
+		}
+	}
 
 }
